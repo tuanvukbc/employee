1ef5373d29f7aa21af40292e568a5a19c2c271c5
tuanvukbc <66425034+tuanvukbc@users.noreply.github.com>
2020-06-19 08:54:04 +0700

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -1,96 +1,47 @@
-#include "Date.h"
-#include "Employee.h"
+#include "date.h"
 
-int format_tt(string tt)    // check format diem danh
+int Date::validate(string& datt)
 {
-    if (tt == "DL" || tt == "DLNN" || tt == "N" || tt == "NP")
-        return 1;
-    else return 0;
-}
-void getDatacsv(string file)
-{
-    fstream myfile;
-    myfile.open(file, ios::in);
-    if (myfile.is_open())
+    stringstream ss(datt);
+    int day, month, year;
+
+    ss >> day >> month >> year;
+    if (year >= 1000 && year <= 3000)
     {
-        string id, tt;
-        cout << "Nhap ma nhan vien \n";
-        getline(cin, id);
-        while (testId(file, id))
-        {
-            cout << "Ma so nhan vien khong khong ton tai\n";
-            cout << "Moi nhap lai ma nhan vien: ";
-            getline(cin, id);
-        }
-        int n;
-        Employee e = searchEmployee(file, id);
-        print(e);
-        cout << "===Diem Danh====\n";
-        cout << "So ngay diem danh \n";
-
-        cin >> n;
-
-        cin.ignore();
-
-        for (int i = 0; i < n; i++)          //
-        {
-            cout << "Nhap ngay thang diem danh ";
-            getline(cin, e.date[i].date);
-            while (valiDate(e.date[i].date) == 0)
-            {
-                cout << "Ban chua nhap dung dinh dang ngay thang, VD: 14 5 1999 \n";
-                cout << "Nhap ngay thang diem danh: ";
-                getline(cin, e.date[i].date);
-            }
-            cout << "1.DL: Di lam \n2.DLNN: di lam nua ngay \n3.N: nghi\n4.NP: nghi phep \n";
-            getline(cin, e.date[i].status);
-            string tt = e.date[i].status;
-            while (format_tt(tt) == 0)
-            {
-                cout << "Ban chua nhap dung dinh dang , VD: NL\n";
-                getline(cin, e.date[i].status);
-                tt = e.date[i].status;
-            }
-            //
-            //Check xem co bi Diem danh trung ngay khong
-            //
-             // check trong qua trinh nhap
-            for (int x = 0; x < i - 1; x++)
-                for (int y = x + 1; y < i; y++)
+        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day > 0 && day <= 31)
+            return 1;
+        else
+            if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 0 && day <= 30)
+                return 1;
+            else
+                if (month == 2)
                 {
-                    if (e.date[x].date == e.date[y].date)
-                    {
-                        cout << e.date[y].date << " Da duoc diem danh, ban co muon sua khong ?\n";
-                        int k;
-                        bool key = true;
-                        cout << "1. Co\n2. Khong\nNhap 1 hoac 2";
-                        cin >> k;
-                        while (key)
-                        {
-                            if (k == 1)key = false;
-                            if (k == 2)
-                            {
-                                getline(cin, e.date[y].status);
-                                int temp = x;
-                                x = temp;
-                                key = false;
-                            }
-                        }
-
-                    }
+                    if ((year % 4 == 0 || year % 400 == 0) && day > 0 && day <= 29)
+                        return 1;
+                    else if (day > 0 && day < 30)return 1;
                 }
-            //check trong file(Chua code)
-
-        }
-        string filepath = "C:/Users/Tuan Vu/Desktop/test.txt";
-                ofstream read(filepath, ios::app);
-                read << e.id << endl;
-                for (int i = 0; i < n; i++)
-                {
-                    read << ',' << e.date[i].date << ':' << e.date[i].status << endl;
-                    if (i == n - 1)read << endl;
-                }
+                else
+                    return 0;
     }
-    else cout << "Mo file that bai. TIP: Kiem tra duong dan hoac Chay chuong trinh voi quyen admin\n";
-
+    else
+        return 0;
+    return 0;
+}
+void Date::nhap()
+{
+    getline(cin, this->date);
+    while (validate(this->date) == 0)
+    {
+        cout << "Ban chua nhap dung dinh dang ngay thang, VD: 14 5 1999 \n";
+        cout << "Nhap ngay thang nam sinh: ";
+        getline(cin, this->date);
+    }
+}
+string Date::getDate()
+{
+    return date;
+}
+void Date::setdate(string date)
+{
+    this->date = date;
 }
